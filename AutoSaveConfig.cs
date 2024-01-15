2f51e7c6c96e7d6823b2247b197780aa3cc2bcad
unfoundbug <iam@theadmin.co.uk>
2024-01-15 11:35:08 +0000

    Initial Move to Github

---
+++ AutoSaveConfig.cs
@@ -0,0 +1,83 @@
+﻿//#define HighLogging
+//#define ExtractLayerMask
+
+using Rewired;
+using System;
+using UnityEngine;
+using RewiredConsts;
+using Voxeland5;
+using HarmonyLib;
+using System.Reflection;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using System.Linq;
+using System.Diagnostics.Eventing.Reader;
+using VLB;
+using JetBrains.Annotations;
+using BepInEx.Configuration;
+
+namespace AutoSaveConfig
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class AutoSaveConfig : BaseUnityPlugin
+    {
+        public const string pluginGuid = "autosaveconfig.nhickling.co.uk";
+        public const string pluginName = "AutoSaveConfig";
+        public const string pluginVersion = "0.0.0.1";
+
+        public const string Config_TimeoutSection = "Config";
+        public const string Config_TimeoutKey = "Timeout";
+
+        public static ConfigEntry<int> AutoSaveTimeout;
+
+        private static ManualLogSource modLogger;
+        InserterDefinition
+        public void Awake()
+        {
+            modLogger = Logger;
+            modLogger.LogInfo($"{pluginName}: Started {pluginVersion}");
+
+            AutoSaveTimeout = ((BaseUnityPlugin)this).Config.Bind<int>(Config_TimeoutSection, Config_TimeoutKey, 300, new ConfigDescription("Timeout in seconds between autosaves", (AcceptableValueBase)(object)new AcceptableValueRange<int>(30, 600), Array.Empty<object>()));
+
+            Harmony harmony = new Harmony(pluginGuid);
+            modLogger.LogInfo($"{pluginName}: Fetching patch references");
+            
+            modLogger.LogInfo($"{pluginName}: Starting Patch");
+            harmony.PatchAll(typeof(AutoSavePatch));
+        }
+
+    }
+
+    class AutoSavePatch 
+    {
+        [HarmonyPatch(typeof(SaveState), "NeedsToAutoSave")]
+        [HarmonyPrefix]
+        public static bool Prefix(ref bool __result)
+        {
+            
+            try
+            {
+                bool isSaver = SettingsMenu.autoSaveEnabled && NetworkConnector.IsHost;
+                if (isSaver)
+                {
+                    __result = SaveState.currentPlayTime - SaveState.instance.metadata.playTime > AutoSaveConfig.AutoSaveTimeout.Value;
+                }
+                else
+                {
+                    __result = false;
+                }
+
+
+                return false;
+            }
+            catch(Exception e)
+            {
+            }
+
+            return true;
+        }
+
+
+    }
+}
---
+++ Packaged/README.md
@@ -0,0 +1,45 @@
+# AutoSaveConfig
+
+## Description
+
+Allows the player to configure the timeout used for autosaves. Configuration is stored in a configuration file which is generated after first using the mod.
+
+Default timeout: 5 minutes
+Available range: 30 seconds to 10 minutes.
+
+### Manual Install Instructions
+
+Note: If you are playing on Gamepass, your game version is likely behind the steam version. Please check the version compatibility chart below.
+
+Your game folder is likely in one of these places:  
+    • Steam: (A-Z):/steam/steamapps/common/Techtonica  
+    • Gamepass: (A-Z):/XboxGames/Techtonica/Content  
+    • Gamepass: Could also be in C:/Program Data/WindowsApps  
+
+1. Download BepInEx v5.4.21 from [here](https://github.com/BepInEx/BepInEx/releases)
+2. Follow the installation instructions [here](https://docs.bepinex.dev/articles/user_guide/installation/index.html)
+3. Extract the contents of the .zip file for this mod.
+4. Drag the "BepInEx" folder into your game folder.
+5. Change config options. 
+
+## compatibility
+| Mod Version | Game Version |
+| --- | --- |
+| 0.0.4 | 0.1.2a |
+| 0.0.3 | Unreliable, do not use |
+| 0.0.2 | Unreliable, do not use |
+| 0.0.1 | Unreliable, do not use |
+
+## Changelog
+
+### 0.0.4
+Patch the auto save to allow configuration reliably.
+
+&nbsp;
+## Disclaimer
+
+Note: NEW Games must be loaded, saved, and reloaded for mods to take effect. Existing saves will auto-apply mods. 
+Please be sure to backup your saves before using mods: AppData\LocalLow\Fire Hose Games\Techtonica 
+USE AT YOUR OWN RISK! Techtonica Devs do not provide support for Mods, and cannot recover saves damaged by mod usage.
+
+Some assets may come from Techtonica or from the website created and owned by Fire Hose Games, who hold the copyright of Techtonica. All trademarks and registered trademarks present in any images are proprietary to Fire Hose Games.
